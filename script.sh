e5af91cb0de5af3cb9a87b36b0e0c22e3052e724
genghispirate <61278121+genghispirate@users.noreply.github.com>
2020-12-07 22:11:33 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#my first script 
+sudo pacman -S firefox chromium anki audacious audacity youtube-dl budgie-desktop  mplayer speedtest-cli sxiv transmission-qt vlc
+yay onlyoffice-bin etcher-bin ulauncher timeshift
