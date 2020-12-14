6931d29989a3803395baee77509e56145707ddd9
genghispirate <61278121+genghispirate@users.noreply.github.com>
2020-12-15 00:33:40 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#my first script 
+#setup script
 mv bashrc /home/host/.bashrc
-sudo pacman -S firefox chromium anki audacious audacity youtube-dl budgie-desktop  mplayer speedtest-cli sxiv transmission-qt vlc
+sudo pacman -S firefox chromium anki audacious audacity youtube-dl budgie-desktop docker mplayer speedtest-cli sxiv transmission-qt vlc
 yay onlyoffice-bin etcher-bin ulauncher timeshift
