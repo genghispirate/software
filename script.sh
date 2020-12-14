3b3666659c7b72827a6e6978bc752be8602ad317
genghispirate <61278121+genghispirate@users.noreply.github.com>
2020-12-15 00:32:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #my first script 
+mv bashrc /home/host/.bashrc
 sudo pacman -S firefox chromium anki audacious audacity youtube-dl budgie-desktop  mplayer speedtest-cli sxiv transmission-qt vlc
 yay onlyoffice-bin etcher-bin ulauncher timeshift
